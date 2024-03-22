08cc34daaf67e63de433a01f52ba0ec728c586f8
AndreiLapotko <shudyx@gmail.com>
2024-03-22 19:23:14 +1000

    Initial commit

---
+++ Seminar01/Task01/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static int getArrayLength(int[] array, int minLen) {
+        if (array.length < minLen) return -1;
+        else return array.length;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int result = getArrayLength(arr1, 10);
+        System.out.println(result);
+    }
+}
---
+++ Seminar01/Task02/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static int getElemByIndexInArr(int[] array, int minLen, int value) {
+        if (array == null) throw new NullPointerException("Массив не должен быть null!");
+        if (array.length < minLen) return -1;
+//        int code2 = -2;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+//                break;
+            }
+        }
+        return -2;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int result = getElemByIndexInArr(null, 10, 8);
+        System.out.println(result);
+    }
+}
