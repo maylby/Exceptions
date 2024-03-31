1c584a52fa9a66b5400d08e94dfd6bb09442005c
Vit <mayllby@gmail.com>
2024-03-31 10:33:03 +0300

    Добавил код

---
+++ Seminar/Sem02/Lesson2/Lesson2.java
@@ -0,0 +1,17 @@
+/*
+Исключения в программировании и их обработка (семинары)
+Урок 2. Исключения и их обработка
+https://gb.ru/lessons/420868
+
+ */
+
+// 00:27:30
+
+package Exceptions.Seminar.Sem02.Lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
