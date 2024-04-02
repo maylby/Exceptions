08ff3d8537ed5b5310ee200353cc8e3b275b3ed6
Vit <mayllby@gmail.com>
2024-04-02 20:56:59 +0300

    Добавил заголовок

---
+++ Seminar/Sem03/task3/Task3.java
@@ -0,0 +1,11 @@
+/*
+Исключения в программировании и их обработка (семинары)
+Урок 3. Продвинутая работа с исключениями в Java
+https://gb.ru/lessons/420869
+
+ */
+package Exceptions.Seminar.Sem03.task3;
+
+public class Task3 {
+    
+}
