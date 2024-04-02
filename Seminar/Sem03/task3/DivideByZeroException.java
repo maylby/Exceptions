a6e95512f8b82b53d0a4d892c4bc2a68eb35caa7
Vit <mayllby@gmail.com>
2024-04-02 21:03:29 +0300

    Добавил код

--- Seminar/Sem03/task3/DivideByZeroException.java
+++ Seminar/Sem03/task3/DivideByZeroException.java
@@ -11,6 +11,8 @@ package Exceptions.Seminar.Sem03.task3;
 Создайте класс исключения, который будет выбрасываться при делении на 0.
 Исключение должно отображать понятное для пользователя сообщение об ошибке.
 */
-public class DivideByZeroException {
-    
+public class DivideByZeroException extends ArithmeticException {
+    public DivideByZeroException() {
+        super("Делить на ноль нельзя");
+    }
 }
