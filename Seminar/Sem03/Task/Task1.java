2797369d2b7f4d013ac17d767cc42136e64591b5
Vit <mayllby@gmail.com>
2024-04-02 18:42:48 +0300

    Добавил код

--- Seminar/Sem03/Task/Task1.java
+++ Seminar/Sem03/Task/Task1.java
@@ -6,6 +6,8 @@ https://gb.ru/lessons/420869
  */
 package Exceptions.Seminar.Sem03.Task;
 
+import java.io.IOException;
+
 /**
 Создайте метод doSomething(), который может быть источником одного
 из типов checked exceptions(тело самого метода прописывать не обязательно).
@@ -13,5 +15,15 @@ package Exceptions.Seminar.Sem03.Task;
 метод doSomething().
 */
 public class Task1 {
-    
+    public static void main(String[] args) {
+        try {
+            doSomething();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void doSomething() throws IOException{
+        throw new IOException(); // для чего нужна эта запись?
+    }
 }
