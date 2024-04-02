6d060f51525427294ce9e47607af16b8afe9fb4f
Vit <mayllby@gmail.com>
2024-04-02 18:33:08 +0300

    Добавил условие

---
+++ Seminar/Sem03/Task/Task1.java
@@ -0,0 +1,11 @@
+package Exceptions.Seminar.Sem03.Task;
+
+/**
+Создайте метод doSomething(), который может быть источником одного
+из типов checked exceptions(тело самого метода прописывать не обязательно).
+Вызовите этот метод из main и обработайте в нем исключение, которое вызвал
+метод doSomething().
+*/
+public class Task1 {
+    
+}
