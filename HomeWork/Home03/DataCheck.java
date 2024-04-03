2aefc9c89a56d3f23cde5d4ab26a9f5b3a24dbae
Vit <mayllby@gmail.com>
2024-04-03 18:29:04 +0300

    Добавил код

--- HomeWork/Home03/DataCheck.java
+++ HomeWork/Home03/DataCheck.java
@@ -8,6 +8,21 @@ https://gb.ru/lessons/420869/homework
  */
 package Exceptions.HomeWork.Home03;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class DataCheck {
     
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String data = scanner.nextLine();
+        String[] arr = data.split(" ");
+        System.out.println(Arrays.toString(arr));
+        // [Фамилия, Имя, Отчество, дата_рождения, номер_телефона, пол]
+        if (arr.length != 6) {
+            // "throw new ..." своё (?) исключение, наследующее "IndexOutOfBoundsException"
+        }
+        
+    }
 }
