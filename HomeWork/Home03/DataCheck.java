9fcdc1ed78344c911a77e8f80daafc2bfcb67203
Vit <mayllby@gmail.com>
2024-04-03 18:07:21 +0300

    Добавил заголовок

---
+++ HomeWork/Home03/DataCheck.java
@@ -0,0 +1,13 @@
+/*
+Исключения в программировании и их обработка (семинары)
+Урок 3. Продвинутая работа с исключениями в Java
+https://gb.ru/lessons/420869/homework
+
+Аттестационная работа
+
+ */
+package Exceptions.HomeWork.Home03;
+
+public class DataCheck {
+    
+}
