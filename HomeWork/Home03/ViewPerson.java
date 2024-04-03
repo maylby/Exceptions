1c955a65392e81c41cf314d4485e6ec5adaef955
Vit <mayllby@gmail.com>
2024-04-03 20:34:04 +0300

    Добавил описание

---
+++ HomeWork/Home03/ViewPerson.java
@@ -0,0 +1,10 @@
+package Exceptions.HomeWork.Home03;
+
+/*
+ * Применить код "ViewCalculator"
+ * из OOP.Homework.Home07.calculator
+ * C:\Users\Vitaliy\GeekBrains\OOP\Homework\Home07\calculator\ViewCalculator.java
+ */
+public class ViewPerson {
+    
+}
