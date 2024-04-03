abac20c7cd30f07633115d9c23d39835465dae6d
Vit <mayllby@gmail.com>
2024-04-03 11:25:14 +0300

    Добавил код

--- Seminar/Sem03/task4/MyArraySizeException.java
+++ Seminar/Sem03/task4/MyArraySizeException.java
@@ -7,5 +7,9 @@ https://gb.ru/lessons/420869
 package Exceptions.Seminar.Sem03.task4;
 
 public class MyArraySizeException {
-    
+    public MyArraySizeException(int x, int y) {
+        super(String.format("В ячейке [%s][%s] - не число", x, y));  // система выдаёт ошибку, 
+                                                                            // ошибки нет
+                                                                            // (?) глюк VSCode
+    }
 }
