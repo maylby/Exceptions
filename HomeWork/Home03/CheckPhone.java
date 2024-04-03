cca152a3f793130acac105038f4441dfde8c5752
Vit <mayllby@gmail.com>
2024-04-03 20:27:23 +0300

    Добавил описание

---
+++ HomeWork/Home03/CheckPhone.java
@@ -0,0 +1,26 @@
+package Exceptions.HomeWork.Home03;
+/*
+ * Телефон массив из 6 цифр от 0 до 9,
+ * где первая цифра не равна нулю
+ * проверка первой цифры на соответствие:
+ * if (n[0] == 0) throw new MyExpt extends IllegalArgumentException
+ * проверка длины номера:
+ * if (fone.length != 6) throw new ... IndexOutOfBoundsException
+ */
+public class CheckPhone {
+
+        // String fone = arr[4];
+        // for (int i = 0; i< arr[4].length; i++) {
+        //     if (arr[4].length != fone.length) {
+        //         throw new MyArraySizeException();
+        //     }
+
+        //     for (int j = 0; j < arr[i].length; j++) {
+        //         try {
+        //             res += Double.parseDouble(arr[i][j]); 
+        //         } catch (IllegalArgumentException e) {
+        //             cache.put(new Point2d(i, j), arr[i][j]);
+        //         }  
+        //     }
+        // }
+}
