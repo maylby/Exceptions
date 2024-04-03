a6e1eb69cbb6460123a8ab8f93fcd6de67ff13f0
Vit <mayllby@gmail.com>
2024-04-03 18:55:31 +0300

    Добавил код

--- HomeWork/Home03/Person.java
+++ HomeWork/Home03/Person.java
@@ -9,5 +9,27 @@ https://gb.ru/lessons/420869/homework
 package Exceptions.HomeWork.Home03;
 
 public class Person {
-    
+    String surname;
+    String name;
+    String patronym;
+    String dateBirth;
+    String phone;
+    String gender;
+
+    public Person(String surname, String name,String patronym, 
+                String dateBirth, String phone, String gender) {
+
+        this.surname = surname; 
+        this.name = name;
+        this.patronym = patronym;
+        this.dateBirth = dateBirth;
+        this.phone = phone;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<%s><%s><%s><%s><%s><%s>%n", 
+                            surname, name, patronym, dateBirth, phone, gender);
+    }
 }
