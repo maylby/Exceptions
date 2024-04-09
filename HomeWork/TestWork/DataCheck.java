fd0152f3ae73bbe4747fd50c11031cfaee7511ee
Vit <mayllby@gmail.com>
2024-04-09 14:31:15 +0300

    Добавил правки

--- HomeWork/TestWork/DataCheck.java
+++ HomeWork/TestWork/DataCheck.java
@@ -25,27 +25,52 @@ public class DataCheck {
         if (arr.length != 6) {
             throw new ArrSizeExep();
         }
+        /**
+         * Проверка даты рождения (birthday)
+         * Соответствие символов числу
+         * Общее количество символов
+         * Количество символов в блоках год, месяц, день
+         */
+        String[] birthday = arr[3].split("."); 
+        System.out.println(birthdayArr.toString(birthday));
+        if (!phone.equals(Integer.number)) {
+            throw new ArgException();
+        } else if (birthdayArr.length != 3) {
+            throw new ArrSizeExep();
+        } else if (birthdayArr[0].length != 4) {
+            throw new ArrSizeExep();
+        } else if (birthdayArr[1][2].length != 2) { 
+            throw new ArrSizeExep();
+        } 
 
+        /**
+         * Проверка символов номера телефона (phone) 
+         * на соответствие числу (Integer)
+         */
         Snring phone = arr[4];
         if (!phone.equals(Integer.number)) {
-            throw new NumberFormatException();
+            throw new ArgException();
         }
 
-        String birthday = arr[3]; // дата рождения
-
+        /**
+         * Проверка заполнения графы пол
+         */
         String gender = arr[5];
         if (!gender.equals("m") || !gender.equals("f")) {
             throw new SexException(gender, gender);
         }
 
+        /**
+         * Запись в файл данных, 
+         * введённых пользователем
+         */
         PersonInfo person = new PersonInfo("Иванов", "Иван", "Иванович", 
                                             "11.11.2001", "123456", "m");
-        System.out.println(p.toString()); // "toString" можно не писать,
-                                          // Java и так понимает, что в консоли
-                                          // Нужно выводить "toString"
+        System.out.println(p.toString()); 
+        java.time.LocalDateTime dateTime = java.time.LocalDateTime.now();
         String fileName = arr[0] + ".txt";
         try (FileWriter fw = new FileWriter(fileName, true);) {
-            fw.write(person.toString() + "\n");
+            fw.write(person.toString() + dateTime + "\n");
             fw.flush();
         } catch (IOException e) {
                 e.printStackTrace();
